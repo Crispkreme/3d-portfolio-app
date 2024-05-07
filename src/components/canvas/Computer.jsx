aa3b38f71eb7ddee1ac2642863f09ee6fe9e7fd5
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-07 10:52:55 -0800

    updated files
    - adding about components
    - adding section wrapper components
    - updating the hero components
    - updating the computer components

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -32,12 +32,15 @@ const ComputerCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
     setIsMobile(mediaQuery.matches);
+
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
+
     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
