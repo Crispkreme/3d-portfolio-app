7906daa7680f239022268bde0e7ba43d84852755
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-07 10:06:04 -0800

    updated file
    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputerCanvas } from './canvas/index';
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-grow items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Marvin</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+          </p>
+        </div>
+      </div>
+
+      <ComputerCanvas />
+    </section>
   )
 }
 
