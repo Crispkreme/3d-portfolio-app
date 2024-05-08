a59b3d76e1411ab1fdf2c569d140c39e8e837d8e
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-08 15:49:04 -0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,8 +17,42 @@ const Contact = () => {
 
   const [loading, isSetLoading] = useState(false);
 
-  const handleChange = (e) => {}
-  const handleSubmit = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    isSetLoading(true);
+
+    emailjs.send(
+      'service_vvnguzl',
+      'template_pc4893u',
+      {
+        from_name: form.name,
+        to_name: 'MarvinTech',
+        from_email: form.email,
+        to_email: 'marvin.tech24@outlook.com',
+        message: form.message,
+      },
+      'FQ3qpkLgYj5WunfFa',
+    ).then(() => {
+      isSetLoading(false);
+      alert('Thank you. I will get back to you as soon as posible');
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      });
+    }, (error) => {
+      isSetLoading(false);
+      console.log(error);
+      alert('Something went wrong');
+    });
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
